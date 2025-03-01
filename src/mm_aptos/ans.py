f51a6e4c076459a5ae4fe2f2fc284de1cef5bbb1
mcbarinov <mcbarinov@proton.me>
2025-03-01 12:47:13 +0300

    ans: can return Ok(None)

--- src/mm_aptos/ans.py
+++ src/mm_aptos/ans.py
@@ -3,15 +3,15 @@ from mm_std import Err, Ok, Result, hr
 
 
 # noinspection DuplicatedCode
-def address_to_primary_name(address: str, timeout: int = 5, proxies: Proxies = None, attempts: int = 3) -> Result[str]:
-    result: Result[str] = Err("not_started")
+def address_to_primary_name(address: str, timeout: int = 5, proxies: Proxies = None, attempts: int = 3) -> Result[str | None]:
+    result: Result[str | None] = Err("not_started")
     url = f"https://www.aptosnames.com/api/mainnet/v1/primary-name/{address}"
     for _ in range(attempts):
         res = hr(url, proxy=random_proxy(proxies), timeout=timeout)
         data = res.to_dict()
         try:
             if res.code == 200 and res.json == {}:
-                return Ok("", data=data)
+                return Ok(None, data=data)
             return Ok(res.json["name"], data=data)
         except Exception as e:
             result = Err(e, data=data)
@@ -19,15 +19,15 @@ def address_to_primary_name(address: str, timeout: int = 5, proxies: Proxies = N
 
 
 # noinspection DuplicatedCode
-def address_to_name(address: str, timeout: int = 5, proxies: Proxies = None, attempts: int = 3) -> Result[str]:
-    result: Result[str] = Err("not_started")
+def address_to_name(address: str, timeout: int = 5, proxies: Proxies = None, attempts: int = 3) -> Result[str | None]:
+    result: Result[str | None] = Err("not_started")
     url = f"https://www.aptosnames.com/api/mainnet/v1/name/{address}"
     for _ in range(attempts):
         res = hr(url, proxy=random_proxy(proxies), timeout=timeout)
         data = res.to_dict()
         try:
             if res.code == 200 and res.json == {}:
-                return Ok("", data=data)
+                return Ok(None, data=data)
             return Ok(res.json["name"], data=data)
         except Exception as e:
             result = Err(e, data=data)
