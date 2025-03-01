a7b6141ee65d6026bc0c254416c65561fd67fc29
mcbarinov <mcbarinov@proton.me>
2025-03-01 11:53:10 +0300

    deps

--- src/mm_aptos/ans.py
+++ src/mm_aptos/ans.py
@@ -1,8 +1,5 @@
-from dataclasses import asdict
-
-import yaml
 from mm_crypto_utils import Proxies, random_proxy
-from mm_std import Err, Ok, Result, hr, run_command
+from mm_std import Err, Ok, Result, hr
 
 
 # noinspection DuplicatedCode
@@ -34,22 +31,4 @@ def address_to_name(address: str, timeout: int = 5, proxies: Proxies = None, att
             return Ok(res.json["name"], data=data)
         except Exception as e:
             result = Err(e, data=data)
-    return result
-
-
-def address_to_names(address: str, cmd: str = "aptos-names", timeout: int = 5, attempts: int = 3) -> Result[list[str]]:
-    result: Result[list[str]] = Err("not_started")
-    cmd = f"{cmd} {address}"
-    for _ in range(attempts):
-        res = run_command(cmd, timeout=timeout)
-        if res.stderr.strip() == "address is not valid":
-            return Err("invalid_address", data=asdict(res))
-        if res.code == 0:
-            try:
-                domains = [d["domain"] for d in yaml.safe_load(res.stdout)]
-                return Ok(domains, data=asdict(res))
-            except Exception as e:
-                result = Err(e, data=asdict(res))
-        else:
-            result = Err(res.stderr or "error", data={"stdout": res.stdout, "stderr": res.stderr, "code": res.code})
     return result
