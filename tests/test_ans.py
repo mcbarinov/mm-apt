dc2827b26520a483719c52f374b49e44d903b565
mcbarinov <mcbarinov@proton.me>
2025-04-19 08:23:48 +0300

    deps

--- tests/test_ans.py
+++ tests/test_ans.py
@@ -15,7 +15,7 @@ async def test_address_to_primary_name():
 
 async def test_address_to_name_async():
     address = "0x68e6982c788b50e3caccc834a4764763381d7201be996914e1310139a35854ed"
-    assert (await ans.address_to_name(address)).ok == "vitalik"
+    assert (await ans.address_to_name(address)).unwrap() == "vitalik"
 
     address = "0xabfabdec0732564bd906fb94e467410a131c6e6040f7bca860458e2026e3b14e"
-    assert (await ans.address_to_primary_name(address)).ok is None
+    assert (await ans.address_to_primary_name(address)).unwrap() is None
